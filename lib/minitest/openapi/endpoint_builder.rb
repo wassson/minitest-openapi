74a8755efdbc491c9e63550be04770c01677c348
Austin Wasson <austinpwasson@gmail.com>
2024-01-02 21:49:15 -0700

    request types

--- lib/minitest/openapi/endpoint_builder.rb
+++ lib/minitest/openapi/endpoint_builder.rb
@@ -4,6 +4,8 @@ module Minitest::OpenAPI
   module EndpointBuilder
     def self.call(context, test)
       request, response = Minitest::OpenAPI::ParseRequest.call(context)
+      return if request.nil?
+
       {
         http_method: request.method,
         status: response.status
--- lib/minitest/openapi/minitest_hook.rb
+++ lib/minitest/openapi/minitest_hook.rb
@@ -3,18 +3,25 @@
 require 'minitest'
 
 module Minitest::OpenAPI
+  REQUEST_TYPES = [:path, :webhook] # TODO: rename
   FileContext = Struct.new(:path)
 
   module RunPatch
     def run(*args)
       result = super
-      if ENV['DOC'] && self.class.document?
-        file_path = result.source_location.first
-        file_ctx = FileContext.new(file_path)
-        export_file_path = Minitest::OpenAPI.path.yield_self { |p| p.is_a?(Proc) ? p.call(file_ctx) : p }
-        endpoint_data = Minitest::OpenAPI::EndpointBuilder.call(self, file_ctx)
+      return result unless ENV['DOC']
+
+      if REQUEST_TYPES.include?(self.class.document_type)
+        puts self.class.document_type
+        test_file_path = result.source_location.first
+        test_file_ctx = FileContext.new(test_file_path)
+
+        export_file_path = Minitest::OpenAPI.path.yield_self { |p| p.is_a?(Proc) ? p.call(test_file_ctx) : p }
+        endpoint_data = Minitest::OpenAPI::EndpointBuilder.call(self, test_file_ctx) || {}
+        # TODO: Change this to work for paths or webhooks
         Minitest::OpenAPI.paths[export_file_path] << endpoint_data
       end
+
       result
     end
   end
@@ -26,22 +33,14 @@ module DocumentClassMethods
   end
 
   module Document
-    def document?
-      @document
+    def document_type
+      @document_type
     end
 
-    def document!
-      @document = true
+    def document!(type = :path)
+      puts "HERE 1"
+      @document_type = type
     end
-
-    # TODO: webhooks
-    # def webhook?
-    #   @webhook
-    # end
-    #
-    # def webhook!
-    #   @webhook = true
-    # end
   end
 end
 
