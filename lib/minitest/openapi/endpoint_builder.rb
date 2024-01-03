76097977379da6b3b23c30c4288cb48073f8687b
Austin Wasson <austinpwasson@gmail.com>
2024-01-03 09:59:22 -0700

    refactor

--- lib/minitest/openapi/endpoint_builder.rb
+++ lib/minitest/openapi/endpoint_builder.rb
@@ -5,16 +5,24 @@ require 'action_dispatch'
 module Minitest
   module OpenAPI
     module EndpointBuilder
-      def self.call(context, _test)
-        request, response = Minitest::OpenAPI::ParseRequest.call(context)
-        return if request.nil?
+      class << self
+        def call(context, _test)
+          @request, @response = Minitest::OpenAPI::ParseRequest.call(context)
+          return if @request.nil?
 
-        endpoint = {}
-        filtered_path = request.filtered_path || 'unknown'
-        request_activity = { http_method: request.method, status: response.status }
+          endpoint = {}
+          filtered_path = @request.filtered_path || 'unknown'
+          request_activity = activity
 
-        endpoint[filtered_path] = request_activity
-        endpoint
+          endpoint[filtered_path] = request_activity
+          endpoint
+        end
+
+        private
+
+        def activity
+          { http_method: @request.method, status: @response.status }
+        end
       end
     end
   end
