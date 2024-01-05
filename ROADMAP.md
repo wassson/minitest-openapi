ee55a2d9691fc6d6a575198eec2b8f9d5f0597fc
Austin Wasson <austinpwasson@gmail.com>
2024-01-05 14:39:10 -0700

    roadmap

---
+++ ROADMAP.md
@@ -0,0 +1,10 @@
+# Roadmap
+
+## Todo
+1. Output `Minitest::OpenAPI.paths` & `Minitest::OpenAPI.webhooks` to file paths
+2. Sorting the paths & webhooks
+3. Testing
+4. Minimum Ruby version support?
+
+## v0.1.0
+TBD
--- lib/minitest/openapi/minitest_hook.rb
+++ lib/minitest/openapi/minitest_hook.rb
@@ -64,8 +64,14 @@ if ENV['DOC']
   Minitest::Test.prepend Minitest::OpenAPI::RunPatch
 
   Minitest.after_run do
+    puts "======================="
+    puts "Paths: "
+    puts "\n"
     pp Minitest::OpenAPI.paths
+    puts "======================="
+    puts "Webhooks: "
     puts "\n"
     pp Minitest::OpenAPI.webhooks
+    puts "======================="
   end
 end
