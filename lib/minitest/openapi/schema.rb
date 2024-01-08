3915ab2b78806e5d534c6376f175c6a51e384af5
Austin Wasson <austinpwasson@gmail.com>
2024-01-07 21:17:55 -0700

    rubocop

--- lib/minitest/openapi/schema.rb
+++ lib/minitest/openapi/schema.rb
@@ -5,15 +5,15 @@ module Minitest
     class InvalidFileFormat < StandardError; end
 
     module Schema
-      ALLOWED_EXTENSIONS = [".json", ".yaml", ".yml"].freeze 
-      
+      ALLOWED_EXTENSIONS = [ ".json", ".yaml", ".yml" ].freeze
+
       def self.build
         file_paths = self.parse_files
 
         Dir.mkdir("docs") unless File.exist?("docs")
         file_paths.each do |file|
           raise InvalidFileFormat unless valid_file_ext?(file)
-          
+
           File.open(file, "w") do |f|
             f.write(JSON.pretty_generate(json_schema(file), indent: "  "))
             puts "File generated!"
@@ -40,16 +40,15 @@ module Minitest
       def self.parse_files
         file_paths = []
         file_paths.concat(
-          Minitest::OpenAPI.paths.keys, 
+          Minitest::OpenAPI.paths.keys,
           Minitest::OpenAPI.webhooks.keys
-        ) 
+        )
         file_paths.uniq
       end
-      
+
       def self.valid_file_ext?(file)
         ALLOWED_EXTENSIONS.include?(File.extname(file))
       end
     end
   end
 end
-
